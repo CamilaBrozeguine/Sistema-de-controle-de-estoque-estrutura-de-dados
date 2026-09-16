7a64be03f62360c504e359d077832b0dc5a6874d
CamilaBrozeguine <camilabrozeguine@gmail.com>
2026-09-16 16:47:38 -0400

    ajuste no arquivo estoque.c na contante MAX_ITENS para 50 , inclusão do campo char codigo_barras[20] struct Produto, ;ajuste na estrutura do produto e correção de bug na exibição da lista de produtos

--- main.c
+++ main.c
@@ -13,8 +13,10 @@ void exibir_menu(void) {
 void listar_produtos(Produto lista[], int total) {
     printf("\n--- Produtos Cadastrados ---\n");
     for (int i = 0; i < total; i++) {
-        // BUG: esqueceram de imprimir o ID e a quebra de linha está inadequada
-        printf("Nome: %s | Preco: R$ %.2f | Qtd: %d", lista[i].nome, lista[i].preco, lista[i].quantidade);
+        // BUG corrigido : esqueceram de imprimir o ID e a quebra de linha está inadequada 
+    
+       printf("ID: %d | Codigo: %s | Nome: %s | Preco: R$ %.2f | Qtd: %d\n",
+               lista[i].id, lista[i].codigo_barras, lista[i].nome, lista[i].preco, lista[i].quantidade);
     }
 }
 
@@ -30,16 +32,20 @@ float calcular_total(Produto lista[], int total) {
 int main(void) {
     Produto estoque[MAX_ITENS];
     int total_produtos = 2;
-
+    
+    //inicialização do codigo de barras para os produtos
     estoque[0].id = 1;
+    strcpy(estoque[0].codigo_barras, "7890001");
     strcpy(estoque[0].nome, "Caderno");
     estoque[0].preco = 15.50;
     estoque[0].quantidade = 10;
 
     estoque[1].id = 2;
+    strcpy(estoque[1].codigo_barras, "7890002");
     strcpy(estoque[1].nome, "Caneta");
     estoque[1].preco = 3.00;
     estoque[1].quantidade = 50;
+
 
     int opcao = -1;
     while (opcao != 0) {
